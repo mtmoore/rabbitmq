66e6b431fb805b4eec3448aac13915a85c94b6a7
Marcus Tyler-Moore <mtmoore1972@gmail.com>
2019-02-18 21:57:34 +0000

    Initial Commit

---
+++ sendToMSQ.py
@@ -0,0 +1,13 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
